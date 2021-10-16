4d3f30696d371aa989d99e7f71000c6222299c9a
Aleksey Potapov <apotapovxx@mail.ru>
2021-10-16 19:00:48 +0300

    lesson5 hw done using web-ui

--- server/src/fmserv.js
+++ server/src/fmserv.js
@@ -1,69 +1,94 @@
 "use strict";
 
-const net = require("net");
-const JsonSocket = require("json-socket");
+const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const mime = require("mime-types");
 
 const port = 5555;
-const server = net.createServer();
-server.listen(port);
-console.log(`Listen to port ${port}`);
-server.on("connection", function (socket) {
-  const jsonSocket = new JsonSocket(socket);
+const rootDir = "/";
 
-  jsonSocket.on("message", function (message) {
+http
+  .createServer((request, response) => {
+    // console.log(`request: ${request.url}`);
+    const reqPath = request.url;
+    let fullPath = path.join(rootDir, reqPath);
     try {
-      // console.log(`Request: ${JSON.stringify(message)}`);
-      let reply;
-      switch (message.command) {
-        case "GET_DIR_CONTENT_LIST":
-          reply = createDirContentListReply(message.payload.path);
-          break;
-        case "GET_FILE_CONTENT":
-          reply = createFileContentReply(message.payload.path);
-          break;
-        default:
-          reply = createUnknownReply();
+      let lstat = fs.lstatSync(fullPath);
+      if (lstat.isSymbolicLink()) {
+        fullPath = followSymlink(fullPath);
       }
-
-      // console.log(`Reply: ${JSON.stringify(reply)}`);
-      jsonSocket.sendMessage(reply);
+      lstat = fs.lstatSync(fullPath);
+      if (lstat.isDirectory()) {
+        const fileList = fs.readdirSync(fullPath);
+        response.setHeader("Content-Type", "text/html");
+        response.end(
+          constructDocument(
+            fileList
+              .map((val) => {
+                const relDirPath = fullPath.slice(rootDir.length);
+                // console.log(`relDirPath = ${relDirPath}`);
+                const relFullPath = path.join(relDirPath, val);
+                // console.log(`relFullPath = ${relFullPath}`);
+                return `<a href=/${relFullPath}>${decodateFilename(
+                  fullPath,
+                  val
+                )}</a>`;
+              })
+              .join("\n<br>\n") || "No data in this folder"
+          )
+        );
+      } else if (lstat.isFile()) {
+        const contentType = mime.contentType(path.extname(fullPath));
+        // console.log(`contentType `,contentType);
+        response.setHeader("Content-Type", contentType || "text/plain");
+        const readStream = fs.createReadStream(fullPath, "utf-8");
+        readStream.pipe(response);
+      } else {
+        response.setHeader("Content-Type", "text/html");
+        response.end(constructDocument("Unsupported filesystem entry"));
+      }
     } catch (error) {
-      console.log(error.toString());
+      console.error(error.toString());
+      response.statusCode = 404;
+      response.end(`Path '${fullPath}' not exit!`);
     }
-  });
-});
+  })
+  .listen(port, "localhost");
 
-function getDirContentList(dirPath) {
-  return fs.readdirSync(dirPath).map((val) => ({
-    fileName: String(val),
-    isDir: fs.lstatSync(path.join(dirPath, val)).isDirectory(),
-  }));
+function constructDocument(bodyHtml) {
+  return `<!doctype html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>A Basic HTML5 Template</title>
+  </head>
+  <body>
+    ${bodyHtml}
+  </body>
+  </html>`;
 }
 
-function createDirContentListReply(path) {
-  return {
-    reply: "DIR_CONTENT_LIST",
-    payload: {
-      path: String(path),
-      list: getDirContentList(path),
-    },
-  };
+console.log(`Lesten to port ${port}`);
+
+function decodateFilename(fullPath, filename) {
+  const filePath = path.join(fullPath, filename);
+  const lstat = fs.lstatSync(filePath);
+
+  if (lstat.isDirectory()) {
+    return `/${filename}`;
+  } else if (lstat.isSymbolicLink()) {
+    return `~${filename}`;
+  } else {
+    return `${filename}`;
+  }
 }
 
-function createFileContentReply(path) {
-  return {
-    reply: "FILE_CONTENT",
-    payload: {
-      path: String(path),
-      data: fs.readFileSync(path, "utf-8"),
-    },
-  };
-}
-
-function createUnknownReply() {
-  return {
-    reply: "UNKNOWN_COMMAND",
-  };
+function followSymlink(path) {
+  let realPath = path;
+  do {
+    realPath = fs.realpathSync(realPath);
+  } while (fs.lstatSync(realPath).isSymbolicLink());
+  return realPath;
 }
